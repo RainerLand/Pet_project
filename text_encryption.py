be871ccfc940dcd475d3114ce3bd2cc9d849eaf5
Egor <Pasikatuk@gmail.com>
2024-04-08 18:05:16 +0300

    over version

---
+++ text_encryption.py
@@ -0,0 +1,11 @@
+import pyAesCrypt
+
+password = input("Введите пароль для файла: ")
+
+
+#encrypt
+pyAesCrypt.encryptFile("data.txt", "data.txt.aes" , password)
+
+#decrypt
+
+pyAesCrypt.decryptFile("data.txt.aes", "dataout.txt", password)
